2eb424727bff0a1e73d39fc215494219aeae3bf6
Ztroid <ztroidmail@gmail.com>
2021-04-20 10:11:46 +0700

    Coba push lagi

--- src/project/pbo/Main.java
+++ src/project/pbo/Main.java
@@ -3,6 +3,7 @@ package project.pbo;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Coba Ganti");
         System.out.println("Hello World");
     }
 }
