25565e818f422255169df7bf26f5cd1672ad4ef6
Ztroid <ztroidmail@gmail.com>
2021-04-20 11:18:01 +0700

    test push nando

--- src/project/pbo/Main.java
+++ src/project/pbo/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Coba Ganti");
         System.out.println("Hello World");
         System.out.println("hello");
+        System.out.println("Nando Goblok");
     }
 }
