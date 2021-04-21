59a5cd5ad69290676902ea684f53c11351e1928b
Ztroid <ztroidmail@gmail.com>
2021-04-21 08:27:15 +0700

    Update cek frame

--- src/project/pbo/GamePanel.java
+++ src/project/pbo/GamePanel.java
@@ -2,14 +2,62 @@ package project.pbo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     public static int width, height;
 
+    private boolean running = false;
+    private Thread thread;
+    private BufferedImage img;
+    private Graphics2D g;
+
     public GamePanel(int width, int height){
+        GamePanel.width = width;
+        GamePanel.height = height;
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         requestFocus();
+    }
+
+    public void addNotify(){
+        super.addNotify();
+
+        if (thread == null){
+            thread = new Thread(this, "GameThread");
+            thread.start();
+        }
+    }
+
+    public void init(){
+        running = true;
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g = (Graphics2D) img.getGraphics();
+    }
+
+
+    @Override
+    public void run() {
+        init();
+
+        while (running){
+            update();
+            render();
+            draw();
+        }
+    }
+
+
+    public void update(){
+    }
+
+    public void render(){
+
+    }
+
+    public void draw(){
+
     }
 }
